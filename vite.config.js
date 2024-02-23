fdbd1c0bd5a055c0bc4fcbe02aa897d002a41034
Paramesh <142381403+SPwaran@users.noreply.github.com>
2024-02-23 21:19:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, open: true },
+  server: { port: 3000,  
+           host: 'react-portopolio.onrender.com',
+           open: true },
 });
